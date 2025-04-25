8a309e1df5ec910f299a391f0face25c634250d7
Pemo0001 <pemo0001@stud.kea.dk>
2025-04-25 12:44:12 +0100

    Marquees

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 "use client";
 import Image from "next/image";
-
+import MarqueeComponent from "./components/marquee";
 
 export default function Home() {
   return (
-    <div className="p-8 flex flex-col items-center justify-center">
+    <div className=" flex flex-col items-center justify-center">
+      <MarqueeComponent text="7/8 - 9/8 2025" />
+      <MarqueeComponent text="VI LUKKER AF FOR SOMMEREN" direction="right" />
       <h1 className="font-exposure text-4xl text-yellow">Your Text Here</h1>
       <Image src="/logo.png" alt="logo" width={1000} height={1000} />
       <p className="mt-4">This text should be in the default font</p>
